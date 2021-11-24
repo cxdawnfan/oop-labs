09865dc76336fd2e16e5186fbd85b3bc7c92bb1a
cxdawnfan <73878739+cxdawnfan@users.noreply.github.com>
2021-11-24 16:07:16 +0200

    lab2

---
+++ lab2/colors.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "pch.h"
+
+static COLORREF red = RGB(255, 0, 0);
+static COLORREF yellow = RGB(255, 255, 0);
+static COLORREF white = RGB(255, 255, 255);
+static COLORREF black = RGB(0, 0, 0);
+static COLORREF green = RGB(0,255, 0);
